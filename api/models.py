c54c49436f8925c79e9017f2c0a0c2f0a69e3729
fluffyfoxtail <fluffyfoxtail988@gmail.com>
2022-02-19 21:32:03 +0300

    init tables and media path

--- api/models.py
+++ api/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    image = models.ImageField(upload_to="photos/%Y/%m/", null=True, blank=True)
+    brand = models.CharField(max_length=200, null=True, blank=True)
+    category = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    numReviews = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    count = models.IntegerField(null=True, blank=True, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True)
+    comment = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.name} got {self.rating} rating'
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    payment_methond = models.CharField(max_length=200, null=True, blank=True)
+    tax_price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    delivery_price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    total_price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    is_paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_delivered = models.BooleanField(default=False)
+    delevered_at = models.DateTimeField(
+        auto_now_add=False, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f' order from {self.user} was made at {self.created_at}'
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    count = models.IntegerField(null=True, blank=True, default=0)
+    # image = models.CharField(max_length=200, null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class DeliveryAddress(models.Model):
+    order = models.OneToOneField(
+        Order, on_delete=models.CASCADE, null=True, blank=True)
+    country = models.CharField(max_length=200, null=True, blank=True)
+    post_index = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    delivery_price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.address
