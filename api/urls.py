d67e9bd607df62ef919f7bee77958fda394bbe51
fluffyfoxtail <fluffyfoxtail988@gmail.com>
2022-02-19 22:09:49 +0300

    start build rest api

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.getRoutes, name="Routes"),
+    path("", views.get_routes, name="Routes"),
+    path("products/", views.get_products, name="products"),
+    path("products/<str:pk>/", views.get_product, name="product"),
+    path("products/create/", views.create_product, name="createProducts"),
+    path("products/<str:pk>/update/", views.update_product, name="updateProducts"),
+    path("products/<str:pk>/delete/", views.delete_product, name="deleteProducts"),
 ]
