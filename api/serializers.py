b5b761d35efd8b01740194274cdbf1af8bc83f34
fluffyfoxtail <fluffyfoxtail988@gmail.com>
2022-02-20 16:54:52 +0300

    fix serializer inheritance

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+
 
 from .models import Product
 
-class ProductSerializer(serializers.Serializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+        
