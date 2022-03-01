8b4a0468bd39b81a8436b474d4f132f7d7805ede
fluffyfoxtail <fluffyfoxtail988@gmail.com>
2022-03-01 21:01:04 +0300

    add forgotten field to order table

--- api/models.py
+++ api/models.py
@@ -42,6 +42,7 @@ class Order(models.Model):
     total_price = models.DecimalField(
         max_digits=7, decimal_places=2, null=True, blank=True)
     is_paid = models.BooleanField(default=False)
+    paid_at = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     is_delivered = models.BooleanField(default=False)
     delevered_at = models.DateTimeField(
