d62a6e75c353de0a57f99c0b8293adad60a61840
fluffyfoxtail <fluffyfoxtail988@gmail.com>
2022-03-04 20:58:52 +0300

    add home template to route and node_modules to gitignore

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
-
+from django.views.generic import TemplateView
 
 from django.conf import settings
 
@@ -10,6 +10,7 @@ urlpatterns = [
     path("api/products/", include("api.urls.product_urls")),
     path("api/users/", include("api.urls.user_urls")),
     path("api/orders/", include("api.urls.order_urls")),
+    path("", TemplateView.as_view(template_name="index.html")),
 ]
 
 if settings.DEBUG:
